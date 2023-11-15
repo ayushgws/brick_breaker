bd696ce71e733547314fce48d66a3e99e6ff61c4
Ayush <ayushgwstele@gmail.com>
2023-11-15 10:38:43 +0530

    Added ScoreManager.cs

---
+++ Assets/Scripts/UI/GameoverScreen.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameoverScreen : MonoBehaviour
+{
+
+    [SerializeField] private Button btnRestart;
+    [SerializeField] private Button btnMenu;
+    [SerializeField] private Button btnContinue;
+
+    [SerializeField] private TextMeshProUGUI txtScore;
+
+    private void Start()
+    {
+        btnRestart.onClick.AddListener(RestartLevel);
+        btnMenu.onClick.AddListener(OpenMenu);
+
+    }
+    
+
+    public void ShowScore()
+    {
+        txtScore.text = ScoreManager.Instance().GetScore().ToString();
+    }
+
+    private void RestartLevel()
+    {
+        SceneLoader.Instance().RestartCurrentLevel();
+    }
+
+    private void OpenMenu()
+    {
+        SceneLoader.Instance().OpenHomeScene();
+    }
+
+}
