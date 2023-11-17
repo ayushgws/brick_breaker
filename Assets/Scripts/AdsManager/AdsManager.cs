5e95c9b76a6ef63fe47b731b3aeef70e1a707a78
Ayush <ayushgwstele@gmail.com>
2023-11-17 16:32:30 +0530

    Added Ads Manager

--- Assets/Scripts/AdsManager/AdsManager.cs
+++ Assets/Scripts/AdsManager/AdsManager.cs
@@ -1,12 +1,14 @@
+using GoogleMobileAds.Api;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AdsManager : MonoBehaviour
 {
-    public static AdsManager instance;
+    private static AdsManager instance;
 
-    private static AdsManager Instance()
+    
+    public static AdsManager Instance()
     {
         return instance;
     }
@@ -22,15 +24,96 @@ public class AdsManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    [SerializeField] private string _interstitialAdUnitId = "ca-app-pub-6761821497536654/4032651330";
 
-    public void ShowRewardAd()
+    [SerializeField] private string _testinterstitialAdID = "ca-app-pub-3940256099942544/1033173712";
+
+    [SerializeField] private bool _testMode;
+    private string _adUnitId;
+
+    
+    private InterstitialAd _interstitialAd;
+
+    /// <summary>
+    /// Loads the interstitial ad.
+    /// </summary>
+    public void LoadInterstitialAd()
     {
+        if (_testMode)
+        {
+            _adUnitId = _testinterstitialAdID;
+        }
+        else
+        {
+            _adUnitId = _interstitialAdUnitId;
+        }
 
+        // Clean up the old ad before loading a new one.
+        if (_interstitialAd != null)
+        {
+            _interstitialAd.Destroy();
+            _interstitialAd = null;
+        }
+
+        Debug.Log("Loading the interstitial ad.");
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // send the request to load the ad.
+        InterstitialAd.Load(_adUnitId, adRequest,
+            (InterstitialAd ad, LoadAdError error) =>
+            {
+                // if error is not null, the load request failed.
+                if (error != null || ad == null)
+                {
+                    Debug.LogError("interstitial ad failed to load an ad " +
+                                   "with error : " + error);
+                    return;
+                }
+
+                Debug.Log("Interstitial ad loaded with response : "
+                          + ad.GetResponseInfo());
+
+                _interstitialAd = ad;
+            });
+
+
+        _interstitialAd.OnAdFullScreenContentClosed += () =>
+        {
+            LoadInterstitialAd();
+        };
     }
 
+
+    private void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            LoadInterstitialAd();
+        });
+    }
+
+    /// <summary>
+    /// Shows the interstitial ad.
+    /// </summary>
     public void ShowInterstitialAd()
     {
-
+        if (_interstitialAd != null && _interstitialAd.CanShowAd())
+        {
+            Debug.Log("Showing interstitial ad.");
+            _interstitialAd.Show();
+        }
+        else
+        {
+            
+            Debug.LogError("Interstitial ad is not ready yet.");
+        }
     }
+
+
+
+
 
 }
