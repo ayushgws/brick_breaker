cc59e7b641a282357cf9cbdaab1ac1bf791fa97e
Hemant <Hemant>
2023-11-25 11:06:04 +0530

    Added Reward Ads

---
+++ Assets/Modules/DailyBonus/Scripts/DailyBonus.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DailyBonus : MonoBehaviour
+{
+
+    [SerializeField] private List<int> Rewards;
+    private int day;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void UpdateReward()
+    {
+
+    }
+
+    public void ClaimReward()
+    {
+
+    }
+
+
+
+}
